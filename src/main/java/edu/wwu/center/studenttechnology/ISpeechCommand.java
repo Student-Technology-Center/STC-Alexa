2ffb7c54fbf4bb0fef5dae7c64c58930e049042b
Alex <AlexAyala9975@hotmail.com>
2017-01-15 14:22:52 -0800

    Remove unused command interface

--- src/main/java/edu/wwu/center/studenttechnology/ISpeechCommand.java
+++
@@ -1,7 +0,0 @@
-package edu.wwu.center.studenttechnology;
-
-import com.amazon.speech.speechlet.SpeechletResponse;
-
-public interface ISpeechCommand {
-    public SpeechletResponse execute(Object... data);
-}
